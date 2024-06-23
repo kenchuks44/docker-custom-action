f39b95cb8c2db45566dbb5aa610ae2409c99a32d
Chukwuka Eze <88329191+kenchuks44@users.noreply.github.com>
2024-06-23 20:57:32 +0100

    edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,12 @@ pull_request_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo "PR Number - $pull_request_number"
 
 # Fetch a random GIF with Giphy API
-giphy_response=$(curl -k "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
+giphy_response=$(curl -s -k "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
 echo "Giphy Response - $giphy_response"
 
-# Extract GIF URL from Giphy response
-# gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
-# echo "GIPHY_URL - $gif_url"
+Extract GIF URL from Giphy response
+gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
+echo "GIPHY_URL - $gif_url"
 
 # Create a comment with the GIF on the PR
 comment_response=$(curl -L \
