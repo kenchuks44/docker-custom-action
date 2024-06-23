cf106eb05efa2c26a554bfa88e2ddb14a7238884
Chukwuka Eze <88329191+kenchuks44@users.noreply.github.com>
2024-06-23 20:59:45 +0100

    edited

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ echo "PR Number - $pull_request_number"
 giphy_response=$(curl -s -k "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_API_KEY&tag=thank%20you&rating=g")
 echo "Giphy Response - $giphy_response"
 
-Extract GIF URL from Giphy response
+# Extract GIF URL from Giphy response
 gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
 echo "GIPHY_URL - $gif_url"
 
